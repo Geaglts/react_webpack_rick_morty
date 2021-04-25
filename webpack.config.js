fbd9e29832ce03ece4f4d27682db54729d53b58a
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-04-25 18:31:51 -0500

    Aplicacion lista para produccion

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const DotEnv = require("dotenv-webpack");
 
 module.exports = {
     entry: "./src/index.js",
